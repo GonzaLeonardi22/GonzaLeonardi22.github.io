4b95087f4b42f773a1b3f6a2ebfbc4b418186329
GonzaLeonardi22 <gonzaleonardis@gmail.com>
2022-05-13 19:09:51 -0300

    creando mi primer sitio web

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector(".menu-btn").addEventListener("click", () =>{
+    document.querySelector(".nav__ul").classList.toggle("show")})
+
+    ScrollReveal().reveal('.header');
+    ScrollReveal().reveal('.container', {delay:400});
+    ScrollReveal().reveal('.section__main', {delay:400});
+    ScrollReveal().reveal('.section2__main', {delay:400});
+    ScrollReveal().reveal('.container__card2', {delay:400});
+    ScrollReveal().reveal('.container__banner', {delay:400});
+    ScrollReveal().reveal('.quienes-somos', {delay:400});
+    ScrollReveal().reveal('.footer', {delay:400});
+
+    
